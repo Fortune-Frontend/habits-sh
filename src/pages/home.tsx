8d2c5674acffe80c6b322d678a0ba784ef588cae
Fortune-Frontend <101920484+Fortune-Frontend@users.noreply.github.com>
2025-08-09 15:06:37 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import { Habit } from "../components/Habit";
 import { CreateModal } from "../components/modals/CreateModal";
 import { Page } from "../components/Page";
 import { useUser } from "../state/user";
-import { loadHabits, saveHabit, incrementStreak } from "/lib/supabaseClient.ts";
+import { loadHabits, saveHabit, incrementStreak } from "../lib/supabaseClient";
 
 
 
